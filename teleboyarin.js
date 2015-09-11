1ef4e7476dd7b054bb54634ea7888ce0015a8c89
Dmitry Ustalov <dmitry@eveel.ru>
2015-09-11 20:08:08 +0500

    Add the /process command

--- teleboyarin.js
+++ teleboyarin.js
@@ -1,7 +1,9 @@
 var TelegramBot = require('node-telegram-bot-api'),
-    request = require('request'),
+    request = require('request')
+    rp = require('request-promise'),
     config = require('./config.json'),
-    raven = require('raven');
+    raven = require('raven'),
+    repl = require('repl');
 
 if (process.env.TELEGRAM_TOKEN) config.token = process.env.TELEGRAM_TOKEN;
 
@@ -32,6 +34,28 @@ function onText(msg) {
                 var reply = processes.map(process => `*${process.id}*: ${process.description}`).join("\n");
                 bot.sendMessage(chatId, reply, {reply_to_message_id: messageId, parse_mode: 'Markdown'});
             });
+            break;
+        case '/process':
+            if (typeof suffix != "undefined" && suffix != null && suffix.length > 0) {
+                var processURL = `${config.apiURL}/processes/${suffix}`,
+                    workersURL = `${config.apiURL}/processes/${suffix}/workers`,
+                    tasksURL   = `${config.apiURL}/processes/${suffix}/tasks`,
+                    answersURL = `${config.apiURL}/processes/${suffix}/answers`;
+                Promise.all([rp.get(processURL), rp.get(workersURL), rp.get(tasksURL), rp.get(answersURL)]).then(responses => {
+                    var process = JSON.parse(responses[0]),
+                        workers = JSON.parse(responses[1]).length,
+                        tasks   = JSON.parse(responses[2]).length,
+                        answers = JSON.parse(responses[3]).length;
+                    var reply = `${process.description}\n*Workers:* ${workers}.\n*Tasks:* ${tasks}.\n*Answers:* ${answers}.`;
+                    bot.sendMessage(chatId, reply, {reply_to_message_id: messageId, parse_mode: 'Markdown'});
+                });
+            } else {
+                request.get(`${config.apiURL}/processes`, function(err, data, body) {
+                    var processes = JSON.parse(body);
+                    var reply = 'Which of the following: ' + processes.map(process => process.id).join(', ') + '?';
+                    bot.sendMessage(chatId, reply, {reply_to_message_id: messageId, parse_mode: 'Markdown'});
+                });
+            }
             break;
         default:
             break;
