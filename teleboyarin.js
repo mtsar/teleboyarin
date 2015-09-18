8320439ea6b17250e0d8486195961fd7dd3af13a
Dmitry Ustalov <dmitry@eveel.ru>
2015-09-18 21:21:48 +0500

    Clean up the code

--- teleboyarin.js
+++ teleboyarin.js
@@ -1,18 +1,22 @@
+'use strict';
+
 const TelegramBot = require('node-telegram-bot-api'),
       rp = require('request-promise'),
       Redis = require('ioredis'),
       config = require('./config.json'),
-      package = require('./package.json'),
-      raven = require('raven'),
-      repl = require('repl');
+      npm = require('./package.json'),
+      raven = require('raven');
 
-if (process.env.SENTRY_DSN) new raven.Client(process.env.SENTRY_DSN).patchGlobal();
+if (process.env.SENTRY_DSN) {
+    const sentry = new raven.Client(process.env.SENTRY_DSN);
+    sentry.patchGlobal();
+}
 
 if (process.env.TELEGRAM_TOKEN) config.token = process.env.TELEGRAM_TOKEN;
 
 const request = rp.defaults({headers: {
-    'User-Agent': `Teleboyarin/${package.version}`,
-    'Accept': 'application/json'
+    'User-Agent': `Teleboyarin/${npm.version}`,
+    Accept: 'application/json'
 }});
 
 const redis = process.env.REDIS_PORT ? new Redis(process.env.REDIS_PORT) : (config.redis ? new Redis(config.redis) : new Redis());
@@ -20,14 +24,15 @@ const redis = process.env.REDIS_PORT ? new Redis(process.env.REDIS_PORT) : (conf
 var workers = {};
 
 function getWorker(process, userId) {
-    if (userId in workers) return new Promise((resolve, reject) => resolve(workers[userId]));
+    if (!(process.id in workers)) workers[process.id] = {};
+    if (userId in workers[process.id]) return new Promise((resolve, reject) => resolve(workers[process.id][userId]));
     const workerGetReq = `${config.apiURL}/processes/${process.id}/workers/tagged/telegram${userId}`,
           workerPostReq = `${config.apiURL}/processes/${process.id}/workers`;
     return new Promise((resolve, reject) =>
         request.get(workerGetReq).
-            then((worker) => resolve(workers[userId] = JSON.parse(worker))).
-            catch((err) => err.statusCode != 404 ? reject(err) : request.post(workerPostReq, {form: {tags: `telegram${userId}`}})).
-                then((worker) => resolve(workers[userId] = JSON.parse(worker))).
+            then((worker) => resolve(workers[process.id][userId] = JSON.parse(worker))).
+            catch((err) => err.statusCode !== 404 ? reject(err) : request.post(workerPostReq, {form: {tags: `telegram${userId}`}})).
+                then((worker) => resolve(workers[process.id][userId] = JSON.parse(worker))).
                 catch((err) => reject(err))
     );
 }
@@ -46,7 +51,7 @@ function onText(msg) {
         return;
     }
 
-    redis.get(msg.from.id).then(raw => {
+    redis.get(msg.from.id).then((raw) => {
         const state = raw ? JSON.parse(raw) : undefined;
 
         if (text == '/cancel') {
@@ -55,18 +60,10 @@ function onText(msg) {
             redis.del(msg.from.id).then(() => bot.sendMessage(msg.chat.id, reply, {reply_markup: markup}));
         } else {
             switch (!!state ? state.text : undefined) {
-                case '/annotate':
-                    stateAnnotate(text, msg, state);
-                    break;
-                case '/annotate/answer':
-                    stateAnnotateAnswer(text, msg, state);
-                    break;
-                case '/process':
-                    stateProcess(text, msg, state);
-                    break;
-                default:
-                    stateInitial(text, msg, state);
-                    break;
+                case '/annotate':        stateAnnotate(text, msg, state);       break;
+                case '/annotate/answer': stateAnnotateAnswer(text, msg, state); break;
+                case '/process':         stateProcess(text, msg, state);        break;
+                default:                 stateInitial(text, msg, state);        break;
             }
         }
     });
@@ -76,110 +73,104 @@ function stateInitial(text, msg, state) {
     switch (text) {
         case '/start':
             bot.sendMessage(msg.chat.id, 'Hi, ' + msg.from.first_name + '!');
-            break;
+        break;
         case '/version':
-            request.get(`${config.apiURL}/version`).then(body => {
+            request.get(`${config.apiURL}/version`).then((body) => {
                 const reply = `Mechanical Tsar v${body}`;
                 bot.sendMessage(msg.chat.id, reply);
             });
-            break;
+        break;
         case '/processes':
-            request.get(`${config.apiURL}/processes`).then(body => {
+            request.get(`${config.apiURL}/processes`).then((body) => {
                 const processes = JSON.parse(body);
-                const reply = processes.map(process => `*${process.id}*: ${process.description}`).join("\n");
+                const reply = processes.map((process) => `*${process.id}*: ${process.description}`).join("\n");
                 bot.sendMessage(msg.chat.id, reply, {parse_mode: 'Markdown'});
             });
-            break;
+        break;
         case '/process':
         case '/annotate':
-            request.get(`${config.apiURL}/processes`).then(body => {
+            request.get(`${config.apiURL}/processes`).then((body) => {
                 const processes = JSON.parse(body);
                 const reply = 'Which one?';
                 const markup = JSON.stringify({
-                      keyboard: processes.map(process => [process.id]),
+                      keyboard: processes.map((process) => [process.id]),
                       one_time_keyboard: true
                 });
                 bot.sendMessage(msg.chat.id, reply, {reply_markup: markup}).then(() => {
                     redis.set(msg.from.id, JSON.stringify({text: text, processes: processes}));
                 });
             });
-            break;
+        break;
         default:
-            break;
+        break;
     }
 }
 
 function stateProcess(text, msg, state) {
-    switch (text) {
-        default:
-            const processReq = request.get(`${config.apiURL}/processes/${text}`),
-                  workersReq = request.get(`${config.apiURL}/processes/${text}/workers`),
-                  tasksReq   = request.get(`${config.apiURL}/processes/${text}/tasks`),
-                  answersReq = request.get(`${config.apiURL}/processes/${text}/answers`);
-            Promise.all([processReq, workersReq, tasksReq, answersReq]).then(responses => {
-                const process = JSON.parse(responses[0]),
-                      workers = JSON.parse(responses[1]).length,
-                      tasks   = JSON.parse(responses[2]).length,
-                      answers = JSON.parse(responses[3]).length;
-                const reply = `[${process.description}](${processReq.uri.href})\n*Workers:* ${workers}.\n*Tasks:* ${tasks}.\n*Answers:* ${answers}.`;
-                const markup = JSON.stringify({hide_keyboard: true});
-                redis.del(msg.from.id).then(() => bot.sendMessage(msg.chat.id, reply, {parse_mode: 'Markdown', reply_markup: markup}));
-            });
-            break;
-    }
+    const processReq = request.get(`${config.apiURL}/processes/${text}`),
+          workersReq = request.get(`${config.apiURL}/processes/${text}/workers`),
+          tasksReq   = request.get(`${config.apiURL}/processes/${text}/tasks`),
+          answersReq = request.get(`${config.apiURL}/processes/${text}/answers`);
+    Promise.all([processReq, workersReq, tasksReq, answersReq]).then((responses) => {
+        const process = JSON.parse(responses[0]),
+              workers = JSON.parse(responses[1]).length,
+              tasks   = JSON.parse(responses[2]).length,
+              answers = JSON.parse(responses[3]).length;
+        const reply = `[${process.description}](${processReq.uri.href})\n*Workers:* ${workers}.\n*Tasks:* ${tasks}.\n*Answers:* ${answers}.`;
+        const markup = JSON.stringify({hide_keyboard: true});
+        redis.del(msg.from.id).then(() => bot.sendMessage(msg.chat.id, reply, {parse_mode: 'Markdown', reply_markup: markup}));
+    });
 }
 
 function stateAnnotate(text, msg, state) {
-    switch (text) {
-        default:
-            const process = state.processes.find((p) => p.id == text);
-            if (process) {
-                getWorker(process, msg.from.id).then((worker) => {
-                    request.get(`${config.apiURL}/processes/${process.id}/workers/${worker.id}/task`).then((response) => {
-                        if (!response || 0 === response.length) {
-                            const reply = 'Thank you, but this process has already been finished.';
-                            const markup = JSON.stringify({hide_keyboard: true});
-                            redis.del(msg.from.id).then(() => bot.sendMessage(msg.chat.id, reply, {parse_mode: 'Markdown', reply_markup: markup}));
-                        } else {
-                            const allocation = JSON.parse(response);
-                            const task = allocation.tasks[0];
-                            const reply = task.description;
-                            const markup = JSON.stringify({
-                                  keyboard: task.answers.map((answer) => [answer]).concat([['/stop']]),
-                                  one_time_keyboard: true
-                            });
-                            bot.sendMessage(msg.chat.id, reply, {parse_mode: 'Markdown', reply_markup: markup}).then(() => {
-                                redis.set(msg.from.id, JSON.stringify({text: '/annotate/answer', process: process, worker: worker, task: task}));
-                            });
-                        }
-                    })
-                });
-            }
-            break;
+    const process = state.processes.find((p) => p.id == text);
+    if (process) {
+        getWorker(process, msg.from.id).then((worker) => {
+            request.get(`${config.apiURL}/processes/${process.id}/workers/${worker.id}/task`).then((response) => {
+                if (!response || 0 === response.length) {
+                    const reply = 'Thank you, but this process has already been finished.';
+                    const markup = JSON.stringify({hide_keyboard: true});
+                    redis.del(msg.from.id).then(() => bot.sendMessage(msg.chat.id, reply, {parse_mode: 'Markdown', reply_markup: markup}));
+                } else {
+                    const allocation = JSON.parse(response);
+                    const task = allocation.tasks[0];
+                    const reply = task.description;
+                    const markup = JSON.stringify({
+                          keyboard: task.answers.map((answer) => [answer]).concat([['/stop']]),
+                          one_time_keyboard: true
+                    });
+                    bot.sendMessage(msg.chat.id, reply, {parse_mode: 'Markdown', reply_markup: markup}).then(() => {
+                        redis.set(msg.from.id, JSON.stringify({text: '/annotate/answer', process: process, worker: worker, task: task}));
+                    });
+                }
+            });
+        });
     }
 }
 
 function stateAnnotateAnswer(text, msg, state) {
     switch (text) {
-        case '/stop':
+        case '/stop': {
             const reply = 'Thank you for your service!';
             const markup = JSON.stringify({hide_keyboard: true});
             redis.del(msg.from.id).then(() => bot.sendMessage(msg.chat.id, reply, {reply_markup: markup}));
-            break;
+        }
+        break;
         default:
-            const process = state.process, worker = state.worker, task = state.task;
-            if (task.answers.indexOf(text) > -1) {
+            if (state.task.answers.indexOf(text) > -1) {
+                const process = state.process, worker = state.worker, task = state.task;
                 const answers = {}; answers[`answers[${task.id}]`] = text;
-                request.patch(`${config.apiURL}/processes/${process.id}/workers/${worker.id}/answers`, {form: answers}).then(response => {
+                request.patch(`${config.apiURL}/processes/${process.id}/workers/${worker.id}/answers`, {form: answers}).then((response) => {
                     const reply = 'Your answer has been recorded!';
                     const markup = JSON.stringify({hide_keyboard: true});
                     bot.sendMessage(msg.chat.id, reply, {parse_mode: 'Markdown', reply_markup: markup}).then(() => {
-                        redis.set(msg.from.id, JSON.stringify({text: process.id, processes: [process]})).then(() =>{
-                            stateAnnotate(process.id);
+                        const stateNew = {text: process.id, processes: [process]};
+                        redis.set(msg.from.id, JSON.stringify(stateNew)).then(() => {
+                            stateAnnotate(stateNew.text, msg, stateNew);
                         });
                     });
                 });
             }
-            break;
+        break;
     }
 }
